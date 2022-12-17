66bfab823411f472f53ec2d692b0e921c7d600b6
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2022-12-17 10:19:16 +0000

    added add functionality

--- functions2.c
+++ functions2.c
@@ -45,3 +45,25 @@ void _swap(stack_t **stack, unsigned int line_number)
 	current->n = current->next->n;
 	current->next->n = temp;
 }
+/**
+ *_add - adds the top two elements of stack
+ *@stack: the stack
+ *@line_number: number of the line
+ *
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = NULL;
+	int sum = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	current = *stack;
+	sum = current->n + current->next->n;
+	current->next->n = sum;
+	_pop(stack, line_number);
+}
